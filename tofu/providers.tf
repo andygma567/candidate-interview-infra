5659d9c6657b93db96a56c6279bb9123f0e07415
andygma567 <andygma567@gmail.com>
2026-03-22 12:33:34 -0500

    chore: clean up unnecessary files

--- tofu/providers.tf
+++ tofu/providers.tf
@@ -2,9 +2,14 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.68"
+      version = ">= 2.68"
     }
   }
-  required_version = "~> 1.5"
+  required_version = "~> 1.10"
+}
+
+provider "digitalocean" {
+  # Authentication token should be set via DIGITALOCEAN_TOKEN environment variable
+  # or through digitalocean provider configuration
 }
 
