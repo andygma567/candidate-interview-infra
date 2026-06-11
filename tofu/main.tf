28604e7091700d6073b830fc943f0a2bbf0150fd
andygma567 <andygma567@gmail.com>
2026-06-10 22:17:11 -0500

    feat: implement TLS private key generation and update user data configuration for interview VM

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,3 +1,7 @@
+resource "tls_private_key" "cluster" {
+  algorithm = "ED25519"
+}
+
 resource "digitalocean_droplet" "interview_vm" {
   count      = var.droplet_count
   name       = "${var.droplet_name_prefix}-${count.index + 1}"
@@ -7,30 +11,35 @@ resource "digitalocean_droplet" "interview_vm" {
   monitoring = var.enable_monitoring
   tags       = var.tags
 
-  user_data = <<-EOF
-    #cloud-config
-    ssh_pwauth: true
-    disable_root: false
-    chpasswd:
-      expire: false
-      users:
-        - name: root
-          password: ${var.root_password}
-          type: text
-  EOF
+  user_data = templatefile("${path.module}/cloud-init.yaml.tftpl", {
+    root_password   = var.root_password
+    ssh_public_key  = trimspace(tls_private_key.cluster.public_key_openssh)
+    ssh_private_key = tls_private_key.cluster.private_key_openssh
+    is_primary      = count.index == 0
+  })
 }
 
 resource "digitalocean_volume" "interview_storage" {
-  count                   = var.droplet_count
-  name                    = "${var.droplet_name_prefix}-${count.index + 1}-storage"
-  region                  = var.region
-  size                    = var.volume_size_gb
-  initial_filesystem_type = "ext4"
-  tags                    = var.tags
+  count  = var.droplet_count
+  name   = "${var.droplet_name_prefix}-${count.index + 1}-storage"
+  region = var.region
+  size   = var.volume_size_gb
+  tags   = var.tags
 }
 
 resource "digitalocean_volume_attachment" "interview_storage_attachment" {
   count      = var.droplet_count
   droplet_id = digitalocean_droplet.interview_vm[count.index].id
   volume_id  = digitalocean_volume.interview_storage[count.index].id
+}
+
+locals {
+  hosts_ini = templatefile("${path.module}/hosts.ini.tftpl", {
+    private_ips = digitalocean_droplet.interview_vm[*].ipv4_address_private
+  })
+}
+
+resource "local_file" "hosts_ini" {
+  content  = local.hosts_ini
+  filename = "${path.module}/generated/hosts.ini"
 }
